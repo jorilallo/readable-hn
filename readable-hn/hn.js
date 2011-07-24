0e3e3242267b1444d1b9276b409d973f32a8387e
Matias Korhonen <matias@kiskolabs.com>
2011-07-24 14:51:34 +0300

    Show the full hostname after each post title. Adds Zepto.js.

---
+++ readable-hn/hn.js
@@ -0,0 +1,16 @@
+(function() {
+  var host, uri, link, hostname,
+      hostname_re = new RegExp('^(?:f|ht)tp(?:s)?://(www.)?([^/]+)', 'im');
+
+  $(document).ready(function() {
+    $("td.title").each(function(idx, el) {
+      link = $(el).find("a");
+      host = $(el).find("span.comhead");
+
+      if (link.length && host.length) {
+        hostname = $(link).attr("href").match(hostname_re)[2].toString();
+        $(host).text(" (" + hostname + ")");
+      }
+    });
+  });
+}).call(this);
